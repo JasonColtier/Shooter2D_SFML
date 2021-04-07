6fe386f9b03811fcaa47d3827c68c7129e39ad0d
Jason <jasoncoltier@gmail.com>
2021-04-07 22:07:38 +0200

    Player movements

--- include/Player.h
+++ include/Player.h
@@ -9,14 +9,16 @@ public:
     sf::Sprite sprite;
 
 
-    
-   Player();
+    Player();
+    ~Player()=default;
     void Tick(int64_t deltaTime) override;
     void Render(sf::RenderWindow* window) override;
 
 private:
-    int offsetAngle = 90;
-    int speed = 100;
-    sf::Vector2f offsetPos = sf::Vector2f(50,50);
-
+    int offsetAngle = 90;//on offset l'angle de rotation de notre sprite
+    float speed = 0.001f;//l'acceleration de notre vaisseau
+    sf::Vector2f offsetPos = sf::Vector2f(50, 50);
+    sf::Vector2f inertia = sf::Vector2f(0, 0);
+    float drag = 0.001f;
+    float maxVelocity = 0.00025f;
 };
