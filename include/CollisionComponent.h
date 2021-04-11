ffb8246c506c15cc380d31a2892d316fcc7dab94
jean baptiste curvat <jb.curvat@gmail.com>
2021-04-11 12:41:46 +0200

    Add class collisionManager and implementation collisionComponent

--- include/CollisionComponent.h
+++ include/CollisionComponent.h
@@ -5,20 +5,55 @@
 #include <SFML/System/Vector2.hpp>
 #include "Component.h"
 
-//struct Segment
-//{
-//	sf::Vector2<float> 
-//};
+enum CollisionChannel
+{
+	Player,
+	Enemy,
+	PlayerProjectile,
+	EnemyProjectile,
+};
 
 class CollisionComponent : Component
 {
 
 public:
 
-	CollisionComponent() = default;
+	CollisionComponent(std::vector<sf::Vector2f> t_Points, CollisionChannel t_Channel, std::vector<CollisionChannel> t_ExcludedCollisionChannel, float* t_Rotation, sf::Vector2f* t_Position)
+		:l_Points(t_Points)
+		, e_Channel(t_Channel)
+		, l_ExcludedCollisionChannel(t_ExcludedCollisionChannel)
+		, rotation(t_Rotation)
+		, position(t_Position)
+	{
+
+	}
+
+
+	std::vector<sf::Vector2f> getPoints()
+	{
+		std::vector<sf::Vector2f> l_pointsInWorld;
+
+		for (auto point : l_Points)
+		{
+			l_pointsInWorld.push_back(*position + point);
+		}
+
+		return 	l_pointsInWorld;
+	}
+
+
+
+
+	CollisionChannel e_Channel;
+	std::vector<CollisionChannel> l_ExcludedCollisionChannel;
 
 private:
 
-	std::vector<sf::Vector2<float>> l_Points;
+	std::vector<sf::Vector2f> l_Points;
+
+	float* rotation;
+	sf::Vector2f* position;
+
 };
+
 #endif //COLLISIONCOMPONENT_H
