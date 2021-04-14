eaeba7bb8731210d5e53b155951666b2c6980367
jean baptiste curvat <jb.curvat@gmail.com>
2021-04-14 19:01:18 +0200

    Systeme de collision

--- include/CollisionComponent.h
+++ include/CollisionComponent.h
@@ -4,19 +4,23 @@
 #include <vector>
 #include <SFML/System/Vector2.hpp>
 #include "Component.h"
+#include <math.h>
+
+#define PI 3.14159265
 
 enum CollisionChannel
 {
-	Player,
-	Enemy,
-	PlayerProjectile,
-	EnemyProjectile,
+	PlayerChannel,
+	EnemyChannel,
+	PlayerProjectileChannel,
+	EnemyProjectileChannel,
 };
 
-class CollisionComponent : Component
+class CollisionComponent : public Component
 {
 
 public:
+	CollisionComponent() = default;
 
 	CollisionComponent(std::vector<sf::Vector2f> t_Points, CollisionChannel t_Channel, std::vector<CollisionChannel> t_ExcludedCollisionChannel, float* t_Rotation, sf::Vector2f* t_Position)
 		:l_Points(t_Points)
@@ -28,31 +32,58 @@ public:
 
 	}
 
+	virtual ~CollisionComponent() override = default;
 
 	std::vector<sf::Vector2f> getPoints()
 	{
 		std::vector<sf::Vector2f> l_pointsInWorld;
 
-		for (auto point : l_Points)
+		for (const auto point : l_Points)
 		{
-			l_pointsInWorld.push_back(*position + point);
+			const auto norm = sqrtf(pow(point.x, 2) + pow(point.y, 2));
+			auto t_point = sf::Vector2f((cosf(*rotation * (PI / 180)) * norm), (sinf(*rotation * (PI / 180)) * norm));
+
+			t_point.x *= 100;
+			t_point.x = truncf(t_point.x);
+			t_point.x /= 100;
+
+			t_point.y *= 100;
+			t_point.y = truncf(t_point.y);
+			t_point.y /= 100;
+
+			l_pointsInWorld.push_back(*position + t_point);
 		}
 
 		return 	l_pointsInWorld;
 	}
 
+	virtual void UpdateComponent() override
+	{
+		return;
+	}
 
+	float GetStartAbscisse() const
+	{
+		return (position->x - radius);
+	}
 
+	float GetEndAbscisse() const
+	{
+		return (position->x + radius);
+	}
+
+	void OnCollision(sf::Vector2f hitPoint, GameObject* otherObject);
 
 	CollisionChannel e_Channel;
 	std::vector<CollisionChannel> l_ExcludedCollisionChannel;
+	float radius;
+	sf::Vector2f* position;
 
 private:
 
 	std::vector<sf::Vector2f> l_Points;
 
 	float* rotation;
-	sf::Vector2f* position;
 
 };
 
---
+++ src/CollisionManager.cpp
@@ -0,0 +1,104 @@
+#include "CollisionManager.h"
+#include <algorithm>
+#include "CollisionComponent.h"
+#include "GameObject.h"
+
+void CollisionManager::UpdateCollision(std::vector<CollisionComponent*>& abscisseTab)
+{
+	SortByAbscisse(abscisseTab);
+
+	for (int i = 0; i < abscisseTab.size(); ++i)
+	{
+		if (!abscisseTab[i]->Owner->isActivated)
+		{
+			break;
+		}
+		for (int j = i + 1; j < abscisseTab.size(); ++j)
+		{
+			if (abscisseTab[i]->GetEndAbscisse() > abscisseTab[j]->GetStartAbscisse() && abscisseTab[j]->Owner->isActivated)
+			{
+				CheckCollision(abscisseTab[i], abscisseTab[j]);
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+}
+
+void CollisionManager::SortByAbscisse(std::vector<CollisionComponent*>& abscisseTab) const
+{
+	auto abscisseSort = [](CollisionComponent* const g1, CollisionComponent* const g2) -> bool
+	{
+		if (g1->Owner->isActivated != g2->Owner->isActivated) return g1->Owner->isActivated > g2->Owner->isActivated;
+		else return (g1->GetStartAbscisse()) < (g2->GetStartAbscisse());
+	};
+
+	std::sort(abscisseTab.begin(), abscisseTab.end(), abscisseSort);
+}
+
+void CollisionManager::CheckCollision(CollisionComponent* g1, CollisionComponent* g2)
+{
+
+	for (CollisionChannel channel : g1->l_ExcludedCollisionChannel)
+	{
+		if (channel == g2->e_Channel)
+		{
+			return;
+		}
+	}
+
+	const auto dist = sqrtf(pow(g2->position->x - g1->position->x, 2) + pow(g2->position->y - g1->position->y, 2));
+	const auto maxDist = g1->radius + g2->radius;
+
+	if (dist < maxDist)
+	{
+		auto l_g1 = g1->getPoints();
+		auto l_g2 = g2->getPoints();
+
+		for (int i = 0; i < l_g1.size(); ++i)
+		{
+			sf::Vector2f* point_A = &l_g1[i];
+			sf::Vector2f* point_B;
+
+			if (i + 1 >= l_g1.size())
+			{
+				point_B = &l_g1[0];
+			}
+			else
+			{
+				point_B = &l_g1[i + 1];
+			}
+
+			for (int j = 0; j < l_g2.size(); ++j)
+			{
+				sf::Vector2f* point_C = &l_g2[j];
+				sf::Vector2f* point_D;
+				if ((j + 1) >= l_g2.size())
+				{
+					point_D = &l_g2[0];
+				}
+				else
+				{
+					point_D = &l_g2[(j + 1)];
+				}
+
+				const auto det = (point_B->x - point_A->x) * (point_C->y - point_D->y) - (point_C->x - point_D->x) * (point_B->y - point_A->y);
+
+				if (det != 0.0f)
+				{
+					const auto t1 = ((point_C->x - point_A->x) * (point_C->y - point_D->y) - (point_C->x - point_D->x) * (point_C->y - point_A->y)) / det;
+					const auto t2 = ((point_B->x - point_A->x) * (point_C->y - point_A->y) - (point_C->x - point_A->x) * (point_B->y - point_A->y)) / det;
+
+					if (t1 <= 1 && t1 >= 0 && t2 <= 1 && t2 >= 0)
+					{
+						const sf::Vector2f hitPoint = sf::Vector2f((point_A->x + t1 * (point_B->x - point_A->x)), point_A->y + t1 * (point_B->y - point_A->y));
+						g1->OnCollision(hitPoint, g2->Owner);
+						g2->OnCollision(hitPoint, g1->Owner);
+					}
+				}
+			}
+		}
+	}
+}
