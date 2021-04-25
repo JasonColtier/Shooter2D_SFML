0202e3edf0d55b5aa57250f36c41062fe70ac0c5
Jean-Baptiste <jb.curvat@gmail.com>
2021-04-25 18:29:23 +0200

    mini modif

--- include/Components/Component.h
+++ include/Components/Component.h
@@ -1,12 +1,15 @@
 #ifndef COMPONENT_H
 #define COMPONENT_H
 
+#include <cstdint>
+
 class GameObject;
 
 class Component
 {
 public:
 
+	virtual ~Component() = default;
 	virtual void TickComponent(int64_t deltaTime = 0) = 0;
 
 	GameObject* Owner = nullptr;
